50875e5823a08f8c526ad4612c758a2621db2896
shriram <sbshriram16@gmail.com>
2018-06-14 02:54:07 +0530

    Automate loop sim - minor change in code

--- memory_manager_test/vdnn_loop_sim/run_network.sh
+++ memory_manager_test/vdnn_loop_sim/run_network.sh
@@ -1,18 +1,18 @@
-echo vdnn_dyn 1>> ../stderr.dat
-echo vdnn_dyn 1>> ../stdout.dat
-./alexnet_test.out dyn p 2>> ../stderr.dat 1>> ../stdout.dat
-echo vdnn_conv_p 1>> ../stderr.dat
-echo vdnn_conv_p 1>> ../stdout.dat
-./alexnet_test.out conv p 2>> ../stderr.dat 1>> ../stdout.dat
-echo vdnn_conv_m 1>> ../stderr.dat
-echo vdnn_conv_m 1>> ../stdout.dat
-./alexnet_test.out conv m 2>> ../stderr.dat 1>> ../stdout.dat
-echo vdnn_all_p 1>> ../stderr.dat
-echo vdnn_all_p 1>> ../stdout.dat
-./alexnet_test.out all p 2>> ../stderr.dat 1>> ../stdout.dat
-echo vdnn_all_m 1>> ../stderr.dat
-echo vdnn_all_m 1>> ../stdout.dat
-./alexnet_test.out all m 2>> ../stderr.dat 1>> ../stdout.dat
+echo vdnn_dyn 1>> stderr.dat
+echo vdnn_dyn 1>> stdout.dat
+./alexnet_test.out dyn p 2>> stderr.dat 1>> stdout.dat
+echo vdnn_conv_p 1>> stderr.dat
+echo vdnn_conv_p 1>> stdout.dat
+./alexnet_test.out conv p 2>> stderr.dat 1>> stdout.dat
+echo vdnn_conv_m 1>> stderr.dat
+echo vdnn_conv_m 1>> stdout.dat
+./alexnet_test.out conv m 2>> stderr.dat 1>> stdout.dat
+echo vdnn_all_p 1>> stderr.dat
+echo vdnn_all_p 1>> stdout.dat
+./alexnet_test.out all p 2>> stderr.dat 1>> stdout.dat
+echo vdnn_all_m 1>> stderr.dat
+echo vdnn_all_m 1>> stdout.dat
+./alexnet_test.out all m 2>> stderr.dat 1>> stdout.dat
 mkdir expt
 cd expt
 mkdir AlexNet_128
