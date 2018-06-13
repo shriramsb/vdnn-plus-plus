80986d8b2f2043c92436ca0fc7b526874d5d2d61
shriram <sbshriram16@gmail.com>
2018-06-13 22:33:36 +0530

    Show smaller pool might work but larger pool might not

--- memory_manager_test/cnmem_merge_head/src/mem_alloc.cu
+++ memory_manager_test/cnmem_merge_head/src/mem_alloc.cu
@@ -5,30 +5,57 @@
 
 using namespace std;
 
+#define CNMEM_GRANULARITY 512
 
 int main(int argc,char **argv)
 {
-  int *device_data,*host_data;
-  FILE *stats;
-  stats=fopen("output_stats.txt","w");
-  size_t size1 = 716973056;
-  cnmemDevice_t cnmem_device;
-  cnmem_device.device = 0;
-  cnmem_device.size = size1;
-  cnmem_device.numStreams = 0;
-  cnmem_device.streams = NULL;
-  cnmem_device.streamSizes = NULL;
+	// int *device_data,*host_data;
+	FILE *stats;
+	stats=fopen("output_stats.txt","w");
+	// size_t size1 = 716973056;
+	cnmemDevice_t cnmem_device;
+	cnmem_device.device = 0;
+	cnmem_device.size = 9 * CNMEM_GRANULARITY;
+	cnmem_device.numStreams = 0;
+	cnmem_device.streams = NULL;
+	cnmem_device.streamSizes = NULL;
 
-  cnmemInit(1, &cnmem_device, 0);
+	cnmemInit(1, &cnmem_device, CNMEM_FLAGS_CANNOT_GROW);
+
+	void *p[5];
+	cnmemMalloc(&p[0], 5 * CNMEM_GRANULARITY, NULL);
+	cnmemPrintMemoryStateTogether(stats, NULL);
+	cnmemMalloc(&p[1], 2 * CNMEM_GRANULARITY, NULL);
+	cnmemPrintMemoryStateTogether(stats, NULL);
+	cnmemMalloc(&p[2], CNMEM_GRANULARITY, NULL);
+	cnmemPrintMemoryStateTogether(stats, NULL);
+	cnmemFree(p[1], NULL);
+	cnmemPrintMemoryStateTogether(stats, NULL);
+	cnmemMalloc(&p[3], CNMEM_GRANULARITY, NULL);
+	cnmemPrintMemoryStateTogether(stats, NULL);
+	cnmemFree(p[0], NULL);
+	cnmemPrintMemoryStateTogether(stats, NULL);
+	cnmemFree(p[2], NULL);
+	cnmemPrintMemoryStateTogether(stats, NULL);
+	if (cnmemMalloc(&p[4], 8 * CNMEM_GRANULARITY, NULL) != CNMEM_STATUS_SUCCESS) {
+		printf("bad size\n");
+	}
+	cnmemPrintMemoryStateTogether(stats, NULL);
+
+	// cnmemMalloc()
+
  
-  size_t size2 = 303063040;
-  size_t size3 = 512;
-  void *p, *q, *r;
-  cnmemMalloc(&p, size1, NULL);
-  cnmemPrintMemoryStateTogether(stats, NULL);
-  cnmemMalloc(&q, size2, NULL);
-  cnmemPrintMemoryStateTogether(stats, NULL);
-  cnmemMalloc(&r, size3, NULL);
-  cnmemPrintMemoryStateTogether(stats, NULL);
+	
+ 
+	// size_t size2 = 303063040;
+	// size_t size3 = 512;
+	// void *p, *q, *r;
+	// cnmemMalloc(&p, size1, NULL);
+	// cnmemPrintMemoryStateTogether(stats, NULL);
+	// cnmemMalloc(&q, size2, NULL);
+	// cnmemPrintMemoryStateTogether(stats, NULL);
+	// cnmemMalloc(&r, size3, NULL);
+	// cnmemPrintMemoryStateTogether(stats, NULL);
+
 
 }
