aad97c74b9df3764d36c1ebda017078c899b7be2
guest01 <sbshriram16@gmail.com>
2018-06-21 00:24:32 +0530

    run network bug fix in base

--- base/run_network_reverse.sh
+++ base/run_network_reverse.sh
@@ -5,6 +5,6 @@ echo base_p 1>> stderr.dat
 echo base_p 1>> stdout.dat
 ./alexnet_test.out p $1 2>> stderr.dat 1>> stdout.dat
 mkdir second_run
-for f in *.dat; do
+for f in *.dat*; do
 	mv $f second_run/$f
 done;
